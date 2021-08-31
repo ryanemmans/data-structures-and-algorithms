38b53f2f64501d0f274b1612fb7b48e2a13ee525
Ryan Emmans <ryanemmans@gmail.com>
2021-08-30 23:17:21 -0700

    Passes test 02.

--- javascript/code-challenges/challenges-11.test.js
+++ javascript/code-challenges/challenges-11.test.js
@@ -38,7 +38,12 @@ For example, count(5, [[1, 3, 5, 7, 9], [5, 5, 5], [1, 2, 3]]) returns 4.
 const count = (target, input) => {
   // Solution code here...
   if (input.length === 0) {
-
+    return 0;
+  } else {
+    const arr = input.map((arr) => {
+      return arr.filter((num) => num === target).length;
+    });
+    return arr.reduce((acc, val) => (acc += val));
   }
 };
 
@@ -186,7 +191,7 @@ describe('Testing challenge 1', () => {
   });
 });
 
-xdescribe('Testing challenge 2', () => {
+describe('Testing challenge 2', () => {
   test('It should return the number of times the input is in the nested arrays', () => {
     expect(count(5, [[1, 3, 5, 7, 9], [5, 5, 5], [1, 2, 3]])).toStrictEqual(4);
     expect(count(3, [[1, 3, 5, 7, 9], [5, 5, 5], [1, 2, 3]])).toStrictEqual(2);
